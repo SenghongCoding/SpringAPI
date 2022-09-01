7f3e4a22cf9d9f92b552d3b8c99cc06c4e57b8fc
hangsenghong <senghonghang@gmail.com>
2022-09-01 10:20:11 +0700

    update and connect to postgresql

--- buildwebspring/src/main/java/com/src/spring/model/Student.java
+++ buildwebspring/src/main/java/com/src/spring/model/Student.java
@@ -1,9 +1,25 @@
 package com.src.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
     private long id;
+    @Column(name="name")
     private String name;
+    @Column(name="email")
     private String email;
+    @Column(name="gender")
     private String gender;
     public long getId() {
         return id;
